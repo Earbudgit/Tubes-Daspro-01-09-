ad49aadb018692d42dfc22051ab7e7b3c6255187
Reyn <117028090+Avereyy@users.noreply.github.com>
2023-04-11 20:19:37 +0700

    Update main.py

--- Reyn/main.py
+++ Reyn/main.py
@@ -1,9 +1,25 @@
 from construct import *
 from command import *
 
-user = newlist(user)
-password = newlist(password)
-role = cekstr(newlist(role))
+def main():
+    username = "pass"
+    role = "none"
+    status = False
+    while status == False:
+        opsi = input(">>> \n")
+        if opsi == "help":
+            help(role)
 
-
+        elif opsi == "login":
+            if username != "pass":
+                print(f'Login gagal!\nAnda telah login dengan username {username}, silahkan lakukan "logout" sebelum melakukan login kembali')
+            else:
+                (username, role) = login(user, password)
+                
+        elif opsi == "logout":
+            if username == "pass":
+                print("Logout gagal! Anda belum login, silahkan login terlebih dahulu sebelum melakukan logout")
+            else:
+                (username, role) = logout()
+    
 main()
